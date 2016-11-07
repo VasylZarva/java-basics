dd38c488e73bebf259f6b9d09bd6d2f815bee7cb
Vasyl Zarva <vasyan.z@gmail.com>
2016-11-07 20:02:41 +0200

    test

---
+++ src/main/java/com/epom/enums/Main.java
@@ -0,0 +1,70 @@
+package com.epom.enums;
+
+/**
+ * @author Vasyl Zarva.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        someLogic(Month.FEBRUARY);
+    }
+
+
+    private static void someLogic(final Month month) {
+
+        switch (month) {
+            case JANUARY:
+                System.out.println("some logic for JANUARY");
+                break;
+            case FEBRUARY:
+            case MARCH: {
+                System.out.println("some logic for MARCH");
+                System.out.println("some logic for MARCH");
+                System.out.println("some logic for MARCH");
+                System.out.println("some logic for MARCH");
+                break;
+            }
+            case APRIL:
+                System.out.println("some logic for APRIL");
+                break;
+            default:
+                System.out.println("default logic");
+        }
+    }
+
+    private static void someLogic(final int month) {
+
+        switch (month) {
+            case 1:
+                System.out.println("some logic for JANUARY");
+                break;
+            case 2:
+            case 3: {
+                System.out.println("some logic for MARCH");
+                System.out.println("some logic for MARCH");
+                System.out.println("some logic for MARCH");
+                System.out.println("some logic for MARCH");
+            }
+            default:
+                System.out.println("default logic");
+        }
+    }
+
+    private static void someLogicc() {
+//        final String month = "aldsf";
+//        switch (month) {
+//            case "1":
+//                System.out.println("some logic for JANUARY");
+//                break;
+//            case "2":
+//            case "3": {
+//                System.out.println("some logic for MARCH");
+//                System.out.println("some logic for MARCH");
+//                System.out.println("some logic for MARCH");
+//                System.out.println("some logic for MARCH");
+//            }
+//            default:
+//                System.out.println("default logic");
+//        }
+    }
+}
--- src/main/java/com/epom/enums/Month.java
+++ src/main/java/com/epom/enums/Month.java
@@ -3,5 +3,47 @@ package com.epom.enums;
 /**
  * @author Vasyl Zarva.
  */
-public class Month {
+public enum Month {
+    JANUARY(1, 31) {
+        @Override
+        public void getMyName() {
+            System.out.println("My name is JANUARY");
+        }
+    },
+    FEBRUARY(2, 28) {
+        @Override
+        public void getMyName() {
+            System.out.println("My name is FEBRUARY");
+        }
+    },
+    MARCH(3, 31) {
+        @Override
+        public void getMyName() {
+            System.out.println("My name is MARCH");
+        }
+    },
+    APRIL(4, 30) {
+        @Override
+        public void getMyName() {
+            System.out.println("My name is APRIL");
+        }
+    };
+
+    private final int id;
+    private final int dayNumber;
+
+    Month(final int id, final int dayNumber) {
+        this.id = id;
+        this.dayNumber = dayNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getDayNumber() {
+        return dayNumber;
+    }
+
+    public abstract void getMyName();
 }
