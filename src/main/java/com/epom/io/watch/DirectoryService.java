3824feeb2519299d35333da730860470e0b44077
Vasyl Zarva <vasyan.z@gmail.com>
2016-11-28 18:06:32 +0200

    io, files: DirectoryService

---
+++ src/main/java/com/epom/io/watch/DirectoryService.java
@@ -0,0 +1,24 @@
+package com.epom.io.watch;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * @author Vasyl Zarva.
+ */
+public interface DirectoryService {
+
+	List<File> searchByName(String name);
+
+	List<File> searchByExtension(String extension);
+
+	Long getDirectorySize(File directory, boolean recursive);
+
+	void appendLinesToFile(File file, List<String> lines);
+
+	void zip(File fileToZip);
+
+	void unZip(File fileToZip);
+
+	void delete(File toDelete);
+}
