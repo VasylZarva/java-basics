d613e45c0931b49d02db4ead2ccad0f323f2eff7
Vasyl Zarva <vasyan.z@gmail.com>
2016-12-04 18:20:20 +0200

    io, files: DirectoryService vadym

--- src/main/java/com/epom/io/test/Main.java
+++ src/main/java/com/epom/io/test/Main.java
@@ -1,18 +1,100 @@
 package com.epom.io.test;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.io.StringWriter;
+
+import com.epom.io.watch.NameFilter;
 
 /**
  * @author Vasyl Zarva.
  */
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		System.out.println("start");
-		System.err.println("error message");
-		System.out.println("--------");
-		int i = System.in.read();
-		System.out.println("entered : " + i);
+	private static final String FILE = "temp.out";
+
+	public static void main(String[] args) {
+		final TestSerial fromMemory = new TestSerial("1234567890", "Matviichenko");
+		write(FILE, fromMemory);
+
+		System.out.println("------");
+
+		TestSerial fromFs = read(FILE);
+
+ 		print(fromMemory);
+		System.out.println("------");
+		print(fromFs);
+	}
+	private static void print(final TestSerial fromFs) {
+		System.out.println("id " + fromFs.getId() );
+		System.out.println("name " + fromFs.getName() );
+		System.out.println("value " + fromFs.getValue() );
+		System.out.println("toString " + fromFs.toString() );
+	}
+
+	private static void write(final String name, final TestSerial o) {
+		try (final FileOutputStream fos = new FileOutputStream(name);
+			 final ObjectOutputStream oos = new ObjectOutputStream(fos);) {
+			oos.writeObject(o);
+			oos.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static TestSerial read(final String name) {
+		try (FileInputStream fis = new FileInputStream(name);
+			 ObjectInputStream oin = new ObjectInputStream(fis);) {
+			return  (TestSerial) oin.readObject();
+		} catch (IOException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+
+	private static class TestSerial implements Serializable {
+
+		private static final long serialVersionUID = 1L;
+
+		private String id;
+
+		private String name;
+
+		private int value;
+
+		public TestSerial(String id, String name) {
+			this.id = id;
+			this.name = name;
+		}
+
+		public String getId() {
+			return id;
+		}
+
+		public void setId(String id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public int getValue() {
+			return value;
+		}
+
+		public void setValue(int value) {
+			this.value = value;
+		}
 	}
 }
--- src/main/java/com/epom/io/watch/DirectoryServiceImpl.java
+++ src/main/java/com/epom/io/watch/DirectoryServiceImpl.java
@@ -17,99 +17,101 @@ import java.util.zip.ZipOutputStream;
 /**
  * @author Vasyl Zarva.
  */
-public class DirectoryServiceImpl implements DirectoryService {
-
-
-	@Override
-	public List<File> search(File directory, NameFilter filter) {
-		if (directory.exists() && directory.isDirectory())  {
-			String[] matches = directory.list(new FilenameFilter() {
-				@Override
-				public boolean accept(File dir, String name) {
-					name.startsWith("test");
-					name.endsWith("test");
-					return false;
-				}
-			});
-		}
-	}
-
-
-	@Override
-	public Long getDirectorySize(File directory, boolean recursive) {
-		if (directory == null || !directory.exists() || !directory.isDirectory()) {
-			return 0L;
-		}
-
-		long length = 0;
-		for (File file : directory.listFiles()) {
-			length = length + file.length();
-		}
-		return length;
-	}
-
-	@Override
-	public void appendLinesToFile(File file, List<String> lines) {
-
-
-	}
-
-	@Override
-	public void zip(File fileToZip) {
-		try {
-			FileOutputStream fileOutputStream = new FileOutputStream(fileToZip.getAbsolutePath() + ".zip");
-			ZipOutputStream zipOutputStream = new ZipOutputStream(fileOutputStream);
-
-			ZipEntry zipEntry = new ZipEntry(fileToZip.getAbsolutePath());
-			zipOutputStream.putNextEntry(zipEntry);
-
-
-			zipOutputStream.closeEntry();
-			zipOutputStream.close();
-
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-	}
-
-
-	@Override
-	public void unZip(File fileToZip) {
+public class DirectoryServiceImpl
+//		implements DirectoryService
+{
+//
+//
+//	@Override
+//	public List<File> search(File directory, NameFilter filter) {
+//		if (directory.exists() && directory.isDirectory())  {
+//			String[] matches = directory.list(new FilenameFilter() {
+//				@Override
+//				public boolean accept(File dir, String name) {
+//					name.startsWith("test");
+//					name.endsWith("test");
+//					return false;
+//				}
+//			});
+//		}
+//	}
+//
+//
+//	@Override
+//	public Long getDirectorySize(File directory, boolean recursive) {
+//		if (directory == null || !directory.exists() || !directory.isDirectory()) {
+//			return 0L;
+//		}
+//
+//		long length = 0;
+//		for (File file : directory.listFiles()) {
+//			length = length + file.length();
+//		}
+//		return length;
+//	}
+//
+//	@Override
+//	public void appendLinesToFile(File file, List<String> lines) {
+//
+//
+//	}
+//
+//	@Override
+//	public void zip(File fileToZip) {
 //		try {
-//			FileInputStream fileInputStream = new FileInputStream(fileToZip.getAbsolutePath() + ".zip");
-//			ZipInputStream zipInputStream = new ZipInputStream(fileInputStream);
+//			FileOutputStream fileOutputStream = new FileOutputStream(fileToZip.getAbsolutePath() + ".zip");
+//			ZipOutputStream zipOutputStream = new ZipOutputStream(fileOutputStream);
 //
 //			ZipEntry zipEntry = new ZipEntry(fileToZip.getAbsolutePath());
-//			ZipInputStream.
+//			zipOutputStream.putNextEntry(zipEntry);
 //
-//			ZipInputStream.closeEntry();
-//			ZipInputStream.close();
+//
+//			zipOutputStream.closeEntry();
+//			zipOutputStream.close();
 //
 //		} catch (FileNotFoundException e) {
 //			e.printStackTrace();
 //		} catch (IOException e) {
 //			e.printStackTrace();
 //		}
-
-
-	}
-
-	@Override
-	public void delete(File toDelete) {
-		if (toDelete == null || !toDelete.isFile())
-			return;
-
-			try {
-				Files.deleteIfExists(Paths.get(toDelete.toURI()));
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-
-
-	}
+//
+//	}
+//
+//
+//	@Override
+//	public void unZip(File fileToZip) {
+////		try {
+////			FileInputStream fileInputStream = new FileInputStream(fileToZip.getAbsolutePath() + ".zip");
+////			ZipInputStream zipInputStream = new ZipInputStream(fileInputStream);
+////
+////			ZipEntry zipEntry = new ZipEntry(fileToZip.getAbsolutePath());
+////			ZipInputStream.
+////
+////			ZipInputStream.closeEntry();
+////			ZipInputStream.close();
+////
+////		} catch (FileNotFoundException e) {
+////			e.printStackTrace();
+////		} catch (IOException e) {
+////			e.printStackTrace();
+////		}
+//
+//
+//	}
+//
+//	@Override
+//	public void delete(File toDelete) {
+//		if (toDelete == null || !toDelete.isFile())
+//			return;
+//
+//			try {
+//				Files.deleteIfExists(Paths.get(toDelete.toURI()));
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//			}
+//
+//
+//	}
 }
 
 
